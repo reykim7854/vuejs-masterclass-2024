4eadb5f413fea095dce8d2b9f2d2fdf4e4be575f
reykim7854 <reykim7124@proton.me>
2024-10-28 16:16:15 +0700

    file based routing

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <router-link :to="{ name: '/projects/[id]', params: { id: 1 } }"
+      >Go to Projects</router-link
+    >
+  </div>
+</template>
---
+++ src/pages/projects/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <router-link to="/">Go to Home</router-link>
+  </div>
+</template>
