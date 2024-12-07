1e0c10d7955af656af2da256d731bf2de7582399
reykim7854 <reykim7124@proton.me>
2024-12-07 08:19:10 +0700

    chapt 3 eps 29

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+</script>
 
 <template>
   <main>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref()
+  ; (async () => {
+    const { data, error } = await supabase.from('projects').select()
+
+    if (error) console.error(error)
+
+    projects.value = data
+
+    console.log('Projects : ', projects.value)
+  })()
+</script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
     <router-link to="/">Go to Home</router-link>
+    {{ projects }}
   </div>
 </template>
