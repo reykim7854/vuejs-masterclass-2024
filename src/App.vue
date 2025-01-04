4d788c1fd7bc246b49797e5f4e2008d95dfbfc9a
reykim7854 <reykim7124@proton.me>
2025-01-04 17:21:23 +0700

    chap 4 eps 40

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,14 @@
 <script setup lang="ts">
-import Input from './components/ui/input/Input.vue'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import TopNavbar from '@/components/Layout/TopNavbar.vue'
+import Sidebar from '@/components/Layout/Sidebar.vue'
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
-    <form class="w-full max-w-96">
-      <Input
-        class="w-full bg-background pl-8"
-        type="text"
-        placeholder="Search..."
-      />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage
-            src="https://github.com/radix-vue.png"
-            alt="@radix-vue"
-          />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
-    <router-view></router-view>
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import './assets/index.css'
+import 'iconify-icon'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './assets/index.css'
 
 import App from './App.vue'
 import router from './router'
