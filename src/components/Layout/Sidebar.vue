312d1a391bc035582b91457387682dbd483585b7
reykim7854 <reykim7124@proton.me>
2025-02-12 06:45:06 +0700

    chap 4 eps 41

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button';
 import SidebarLinks from './SidebarLinks.vue'
 
 const links = [
@@ -45,13 +46,13 @@ const accountLinks = [
     <div
       class="flex h-16 items-center border-b px-2 lg:px-4 shrink-0 gap-1 justify-between"
     >
-      <button variant="outline" size="icon" class="w-8 h-8">
+      <Button variant="outline" size="icon" class="w-8 h-8">
         <iconify-icon icon="lucide:menu"></iconify-icon>
-      </button>
+      </Button>
 
-      <button variant="outline" size="icon" class="w-8 h-8">
+      <Button variant="outline" size="icon" class="w-8 h-8">
         <iconify-icon icon="lucide:plus"></iconify-icon>
-      </button>
+      </Button>
     </div>
 
     <nav class="flex flex-col gap-2 justify-between h-full relative">
