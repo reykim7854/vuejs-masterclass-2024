4fcdbdf7277dc27ba92548d48f8d06d9f9916dba
reykim7854 <reykim7124@proton.me>
2025-03-01 13:18:10 +0700

    chap 5 eps 51

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <AuthLayout>
-    <RouterView />
+
+    <RouterView v-slot="{ Component, route }">
+      <Suspense  v-if="Component">
+        <Component :is="Component" :key="route.name"></Component>
+
+        <template #fallback>
+          <span>Loading...</span>
+        </template>
+      </Suspense>
+    </RouterView>
   </AuthLayout>
 </template>
