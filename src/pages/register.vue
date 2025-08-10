a8572db1ec85df1ef3786552775e0c99d62ba5b0
reykim7854 <reykim7124@proton.me>
2025-08-10 13:53:10 +0700

    chap 7 eps 73

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -10,6 +10,8 @@ const formData = ref({
   confirmPassword: '',
 })
 
+const router = useRouter()
+
 const signup = async () => {
   const { data, error } = await supabase.auth.signUp({
     email: formData.value.email,
@@ -18,7 +20,17 @@ const signup = async () => {
 
   if (error) return console.log(error)
 
-  console.log(data)
+  if (data.user) {
+    const { error } = await supabase.from('profiles').insert({
+      id: data.user.id,
+      username: formData.value.username,
+      full_name: `${formData.value.firstName} ${formData.value.lastName}`,
+    })
+
+    if (error) return console.log('Profiles err: ', error)
+  }
+
+  router.push('/')
 }
 </script>
 
