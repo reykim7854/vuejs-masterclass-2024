881c0b9386b1d232d544aa7d512d4ddce9c8b5dc
reykim7854 <reykim7124@proton.me>
2025-09-28 07:48:54 +0700

    chap 7 eps 82

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -33,6 +33,12 @@ const accountLinks = [
     icon: 'lucide:log-out',
   },
 ]
+
+const executeAction = (linkTitle: string) => {
+  if (linkTitle === 'Sign Out') {
+
+  }
+}
 </script>
 
 <template>
@@ -57,7 +63,7 @@ const accountLinks = [
       </div>
 
       <div class="border-y text-center bg-background py-3">
-        <SidebarLinks :links="accountLinks" />
+        <SidebarLinks :links="accountLinks" @actionClicked="executeAction" />
       </div>
     </nav>
   </aside>
